70afbb8e934cb670484a3960edf48e3f2e1758a0
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-07-18 15:02:48 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,44 +5,30 @@ def main():
 
     # Menu options
     options = ["Home 🏠", "Upload 📤", "Tasks 📝", "Settings ⚙️"]
+    selected_option = st.radio("", options, format_func=lambda x: x.split(' ')[0], key='menu')
 
     # Display the selected item
-    selected_option = st.empty()
-    selected_option.write(f"Selected: {options[0]}")
+    st.write(f"Selected: {selected_option}")
 
     # CSS styling for the menu bar
     st.markdown("""
         <style>
-            .menu-bar {
-                display: flex;
-                justify-content: center;
-                background-color: #f0f0f0;
-                padding: 10px 0;
-                border-radius: 5px;
+            .radio span {
+                display: inline-flex;
+                align-items: center;
             }
-            .menu-button {
-                margin: 0 10px;
-                cursor: pointer;
+            .radio span::before {
+                content: "";
+                width: 1em;
+                height: 1em;
+                margin-right: 0.5em;
+                display: inline-block;
+                border-radius: 50%;
+                background-color: #007bff;
+                box-shadow: 0 0 0 3px #fff;
             }
         </style>
     """, unsafe_allow_html=True)
-
-    # Create a horizontal menu bar
-    with st.container():
-        st.write("", options[0], key='home')
-        st.write("", options[1], key='upload')
-        st.write("", options[2], key='tasks')
-        st.write("", options[3], key='settings')
-
-    # Handle button clicks
-    if st.button(options[0], key='Home'):
-        selected_option.write(f"Selected: {options[0]}")
-    if st.button(options[1], key='Upload'):
-        selected_option.write(f"Selected: {options[1]}")
-    if st.button(options[2], key='Tasks'):
-        selected_option.write(f"Selected: {options[2]}")
-    if st.button(options[3], key='Settings'):
-        selected_option.write(f"Selected: {options[3]}")
 
 if __name__ == "__main__":
     main()
