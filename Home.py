9f909927b7b89d3f7a37e40901b08df80652e675
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-07-18 15:20:37 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 selected = option_menu(
   menu_title=None,  # required
   options=["Intro", "Learning", "Implementation"],  # required
-  icons=["🏠", "🧑‍🏫", "👨‍🎨"],  # optional
+  icons=["home", "book", "pallate"],  # optional
   menu_icon="cast",  # optional
   default_index=0,  # optional
   orientation="horizontal",
