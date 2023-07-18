be57775508999429fab18d56fbaa6f69681c4574
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-07-18 14:43:04 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,16 +1,24 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    selected = option_menu(
-        None,
-        ["Home", "Learning", "Implementation"],
-        icons=['🏠', '🧑‍🏫', '👨‍🎨'],
-        menu_icon="cast",
-        default_index=0,
-        orientation="horizontal"
-    )
+def main():
+    with st.sidebar:
+        selected = st.radio(
+            "Select Page",
+            ["Home 🏠", "Learning 🧑‍🏫", "Implementation 👨‍🎨"],
+            index=0,
+            format_func=lambda page: page.split(' ')[0]
+        )
 
-selected
+    if "Home" in selected:
+        st.title('Home Page')
+        # Your home page content here
+    elif "Learning" in selected:
+        st.title('Learning Page')
+        # Your learning page content here
+    elif "Implementation" in selected:
+        st.title('Implementation Page')
+        # Your implementation page content here
 
+if __name__ == "__main__":
+    main()
 
