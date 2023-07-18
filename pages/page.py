a157cca54526dd35557b537b63259b5b5025e1ca
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-07-18 13:02:03 +0700

    Update page.py

--- pages/page.py
+++ pages/page.py
@@ -1,16 +1,14 @@
-from st_pages import Page, add_page_title, show_pages
+# app.py
+from st_pages import Page, show_pages
 
-"## Declaring the pages in your app:"
-
-show_pages(
-    [
-        Page("pages/1_ 🏠_Home.py", "Home", "🏠"),
-        # Can use :<icon-name>: or the actual icon
-        Page("pages/2_📝_Tutorial.py", "Tutorial", ":books:"),
-        # The pages appear in the order you pass them
-        Page("pages/3_🔧_Tools.py", "Example Four", "📖"),
-        Page("pages/4_🕵️‍♀️_Real-case.py", "Example Two", "✏️"),
-        Page("pages/5_📂_Resources.py", "Example Five", "🧰"),
-    ]
-)
+with st.echo("below"):
+    show_pages(
+        [
+            Page("pages/Home.py", "Home", "🏠"),
+            Page("pages/Tutorial.py", "Tutorial", ":books:"),
+            Page("pages/Tools.py", "Example Four", "📖"),
+            Page("pages/Real-case.py", "Example Two", "✏️"),
+            Page("pages/Resources.py", "Example Five", "🧰"),
+        ]
+    )
 
