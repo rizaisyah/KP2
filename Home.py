ec3f4045a05b467f9ce5b5586688cc98ab579bd2
Nikita18701 <88786691+Nikita18701@users.noreply.github.com>
2023-07-18 14:38:52 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,9 +2,14 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-selected = option_menu(None, ["Home", "Learning", "Implementation"], 
-    icons=['🏠', '🧑‍🏫', "👨‍🎨], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
+    selected = option_menu(
+        None,
+        ["Home", "Learning", "Implementation"],
+        icons=['🏠', '🧑‍🏫', '👨‍🎨'],
+        menu_icon="cast",
+        default_index=0,
+        orientation="horizontal"
+    )
 
 selected
 
